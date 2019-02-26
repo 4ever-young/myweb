c3199cfabde1b06d0dfc6c764f2f045fa3c1a050
4ever-young <4ever_17@mail.ru>
2019-02-26 19:15:37 +0400

    1step

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    cout << "Prog Sum!" << endl;
+    int a,b;
+    cin >> a; cin >> b;
+    cout << "Result" << a+b << endl;
+    getchar(); getchar();
+    return 0;
+}
